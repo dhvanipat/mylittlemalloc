c71091e035b20a62405ab81bd93bd2b69c7027f7
Dhvani G. Patel <dgp69@ilab1.cs.rutgers.edu>
2023-10-07 18:48:32 -0400

    Lots of things see desc
    Added makefile compile with make from now on, added gitignore, fixed mymalloc.h

--- mymalloc.h
+++ mymalloc.h
@@ -4,6 +4,8 @@
 #define malloc(s) mymalloc(s, __FILE__, __LINE__)
 #define free(p) myfree(p, __FILE__, __LINE__)
 
+typedef unsigned long int size_t;
+
 void *mymalloc(size_t size, char *file, int line);
 void myfree(void *ptr, char *file, int line);
 
