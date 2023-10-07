327276f695f075f076cdffd622780cc2d0f07898
Ananya Balaji <ab2389@ilab1.cs.rutgers.edu>
2023-10-07 16:57:18 -0400

    struct and free

--- mymalloc.c
+++ mymalloc.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 #include "mymalloc.h"
@@ -6,11 +5,39 @@
 #define MEMLENGTH 512
 static double memory[MEMLENGTH];
 
+struct chunkheader {
+   size_t size; //size of the payload
+   int is_allocated; //chunk is allocated or not
+};
+
+void memory_block(size_t block_size) {
+   //block_size = (block_size + 7) & ~7; //ensures alignment of 8 bytes
+
+   void* memory_block = malloc(MEMLENGTH);
+
+   //create the first chunk as a free block
+   struct chunkheader* first_chunk = (struct chunkheader*) memory_block;
+   first_chunk->size = MEMLENGTH - sizeof(struct chunkheader);
+   first_chunk->is_allocated = 0;
+}
+
 void* malloc(int){
     //(size + 7) & ~7
 }
 
-void free(void*){
+void free(void* ptr){
+
+   //edge cases (like ptr is null, never allocated, etc)
+   if (ptr == NULL){
+      return;
+   }
+
+   //retrieve header of chunk to be freed
+   struct chunkheader* freed_chunk = (struct chunkheader*)((char*)ptr - sizeof(struct chunkheader));
+   freed_chunk->is_allocated = 0;
+
+   //coalesce -  needs to be edited
+   coalesce();
 
 }
 
